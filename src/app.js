fa7f9f2d5a70b8abcf2aaae56367790e28b17334
Leandro Licata <leandrolicata1@gmail.com>
2024-04-05 10:56:48 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const mainRouter = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
 // middlewares
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
