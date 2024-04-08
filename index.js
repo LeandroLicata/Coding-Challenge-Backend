9332605ae7e2aa78f5397263c8a071fa3e5b13c8
Leandro Licata <leandrolicata1@gmail.com>
2024-04-08 04:34:33 -0300

    3002

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
 const { sequelize } = require("./src/db");
 
-app.listen(3001, () => {
+app.listen(3002, () => {
   sequelize.sync({ alter: true });
-  console.log("Listening on port 3001");
+  console.log("Listening on port 3002");
 });
